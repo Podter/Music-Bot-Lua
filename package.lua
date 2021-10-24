ab60c32cba058d2399f0c806550afb4f23eb7941
Podter <podter045@gmail.com>
2021-10-24 13:58:09 +0700

    commit

--- package.lua
+++ package.lua
@@ -3,7 +3,7 @@
     version = "0.0.1",
     description = "A Discord Music Bot written in Lua",
     tags = { "lua", "lit", "luvit" },
-    license = "MIT",
+    license = ".",
     author = { name = "Podter", email = "." },
     homepage = "https://github.com/Podter/Music-Bot-Lua",
     dependencies = {},
